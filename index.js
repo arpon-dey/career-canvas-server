bf55e24dbbac2914b16554d0c70deaf4062417c2
arpon-dey <arpondeydurjoy@gmail.com>
2023-11-06 13:34:39 +0600

    job details route created

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bpsqjlp.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,29 +23,34 @@ async function run() {
   try {
     await client.connect();
 
-    const jobCollection = client.db('jobsDB').collection('jobs');
+    const jobCollection = client.db("jobsDB").collection("jobs");
 
     // app.get('/jobs', async (req, res) => {
     //     const result = await jobCollection.find().toArray();
     //     res.send(result);
     //   });
 
-      app.get('/jobs', async(req, res)=>{
-        const email = req.query.email
-        let query = {}
-        if(email){
-            query= {email:email}
-        }
-        const result = await jobCollection.find(query).toArray()
-        res.send(result)
+    app.get("/jobs", async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { email: email };
+      }
+      const result = await jobCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/jobs/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
 
-      })
-  
-      app.post('/jobs', async (req, res) => {
-        const job = req.body;
-        const result = await jobCollection.insertOne(job);
-        res.send(result);
-      });
+    app.post("/jobs", async (req, res) => {
+      const job = req.body;
+      const result = await jobCollection.insertOne(job);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -62,7 +65,6 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-
 
 app.listen(port, () => {
   console.log(`server started on ${port}`);
